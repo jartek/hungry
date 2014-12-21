9b45a7a93c8a8304c2eaabf9f0e0725c14464b39
jartek <jartek.91@gmail.com>
2014-12-21 10:09:06 +0530

    Pundit error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include Pundit
   include ActionController::MimeResponds
   include ActionController::ImplicitRender
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized(exception)
+    head :forbidden
+  end
 end
